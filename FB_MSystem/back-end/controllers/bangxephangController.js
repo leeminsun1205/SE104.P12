69ec6148eabc05d14ffcc0e39224d4fbf52c71ad
MinSun05 <minhnhut12052004@gmail.com>
2024-12-25 10:25:48 +0700

    update

--- FB_MSystem/back-end/controllers/bangxephangController.js
+++ FB_MSystem/back-end/controllers/bangxephangController.js
@@ -1,4 +1,4 @@
-const { BangXepHang, DoiBong, VongDau } = require('../models');
+const { BangXepHang, DoiBong, VongDau, MG_DB_CT } = require('../models');
 
 const BangXepHangController = {
     async getByMuaGiai(req, res) {
@@ -9,17 +9,24 @@ const BangXepHangController = {
                 include: [
                     { model: DoiBong, as: 'DoiBong' },
                     { model: VongDau, as: 'VongDau' },
+                    {
+                        model: MG_DB_CT, // Model has to be defined correctly
+                        as: 'MG_DB_CT',   // Ensure alias is correct
+                        include: [
+                            { model: DoiBong, as: 'DoiNha', attributes: ['TenDoiBong'] },
+                            { model: DoiBong, as: 'DoiKhach', attributes: ['TenDoiBong'] }
+                        ]
+                    }
                 ],
                 order: [['DiemSo', 'DESC']]
             });
-            
+
             if (bangXepHang.length === 0) {
                 return res.status(404).json({ message: 'Không tìm thấy bảng xếp hạng cho mùa giải này.' });
             }
 
             res.status(200).json(bangXepHang);
         } catch (error) {
-            console.error(error);
             res.status(500).json({ error: error.message });
         }
     },
@@ -29,7 +36,17 @@ const BangXepHangController = {
             const { MaVongDau } = req.params;
             const bangXepHang = await BangXepHang.findAll({
                 where: { MaVongDau },
-                include: [{ model: DoiBong, as: 'DoiBong' }],
+                include: [
+                    { model: DoiBong, as: 'DoiBong' },
+                    {
+                        model: MG_DB_CT,  // Model has to be defined correctly
+                        as: 'MG_DB_CT',   // Ensure alias is correct
+                        include: [
+                            { model: DoiBong, as: 'DoiNha', attributes: ['TenDoiBong'] },
+                            { model: DoiBong, as: 'DoiKhach', attributes: ['TenDoiBong'] }
+                        ]
+                    }
+                ],
                 order: [['DiemSo', 'DESC']]
             });
 
@@ -43,6 +60,5 @@ const BangXepHangController = {
         }
     },
 };
-
 
 module.exports = BangXepHangController;
